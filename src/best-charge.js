f66dc56ff7ef4d2f17ac2d81becda5d2799cd8f0
“your_uLiDongpeng1 <l>
2019-07-10 23:00:11 +0800

    createsomefunction

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,17 @@
+function convertSelectedItemToItemIdCount(selectedItems) {
+  return selectedItems.map((selectedItem) => {
+    const arr = selectedItem.split(' x ');
+    return {
+      id: arr[0],
+      count: Number(arr[1]),
+    };
+  });
+}
+
 function bestCharge(selectedItems) {
   return /*TODO*/;
+}
+
+module.exports = {
+  convertSelectedItemToItemIdCount,
 }
